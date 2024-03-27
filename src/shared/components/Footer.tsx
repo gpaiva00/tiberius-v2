6d87e8b7653ba7a90b96b4c32efb567e5ef204c9
Paiva <gabriel.paiva@orla.tech>
2024-03-26 23:52:42 -0300

    fix: navigating using Link

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import { ABOUT_ROUTE, HELP_ROUTE, HOME_ROUTE } from '@/shared/constants'
 
 import { ListChecks } from 'lucide-react'
@@ -6,29 +8,29 @@ function Footer() {
   return (
     <footer className="absolute bottom-0 flex w-full items-center justify-between border-t border-t-zinc-100 px-4 py-2 shadow">
       <div className="flex items-center gap-2">
-        <a
-          href={HOME_ROUTE}
+        <Link
+          to={HOME_ROUTE}
           className="flex items-center gap-1 text-xs font-semibold text-zinc-700 hover:underline md:text-sm"
         >
           <ListChecks size={16} />
           Tiberius
-        </a>
+        </Link>
         {/* <p className="text-xs text-zinc-400 md:text-sm">{new Date().getFullYear()}</p> */}
         <p className="hidden text-xs text-zinc-400 md:block md:text-sm">Concentre-se no presente</p>
       </div>
       <div className="flex items-center gap-6 md:gap-10">
-        <a
-          href={HELP_ROUTE}
+        <Link
+          to={HELP_ROUTE}
           className="text-xs text-zinc-400 hover:underline md:text-sm"
         >
           Ajuda
-        </a>
-        <a
-          href={ABOUT_ROUTE}
+        </Link>
+        <Link
+          to={ABOUT_ROUTE}
           className="text-xs text-zinc-400 hover:underline md:text-sm"
         >
           Sobre
-        </a>
+        </Link>
         <a
           href="https://forms.gle/ruRh4v4UHHMthkQ38"
           className="text-xs text-zinc-400 hover:underline md:text-sm"
