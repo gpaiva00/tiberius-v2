9c1825ef859770cc18663e1a22b480e8de1dd56f
Gabriel Alves de Paiva <gabriel.paiva@fastsolutions.com.br>
2024-09-04 17:28:50 -0300

    fix: footer text color

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -10,13 +10,13 @@ function Footer() {
       <div className="flex items-center gap-2">
         <Link
           to={HOME_ROUTE}
-          className="flex items-center gap-1 text-xs font-semibold text-zinc-700 underline-offset-4 hover:underline md:text-sm dark:text-zinc-500"
+          className="flex items-center gap-1 text-xs font-semibold text-zinc-700 underline-offset-4 hover:underline dark:text-zinc-500 md:text-sm"
         >
           <ListChecks size={16} />
           Tiberius
           {/* <p className="text-xs text-zinc-400 md:text-sm">{new Date().getFullYear()}</p> */}
           <span className="hidden font-extrabold text-zinc-500 md:block">{'·'}</span>
-          <p className="hidden text-xs font-normal text-zinc-400 md:block md:text-sm dark:text-zinc-500">
+          <p className="hidden text-xs font-normal text-zinc-500 dark:text-zinc-500 md:block md:text-sm">
             Concentre-se no presente
           </p>
         </Link>
@@ -24,25 +24,25 @@ function Footer() {
       <div className="flex items-center gap-6 md:gap-10">
         <Link
           to={HELP_ROUTE}
-          className="text-xs text-zinc-400 underline-offset-4 hover:underline md:text-sm dark:text-zinc-500"
+          className="text-xs text-zinc-500 underline-offset-4 hover:underline dark:text-zinc-500 md:text-sm"
         >
           Ajuda
         </Link>
         <Link
           to={ABOUT_ROUTE}
-          className="text-xs text-zinc-400 underline-offset-4 hover:underline md:text-sm dark:text-zinc-500"
+          className="text-xs text-zinc-500 underline-offset-4 hover:underline dark:text-zinc-500 md:text-sm"
         >
           Sobre
         </Link>
         <a
           href="https://paivacodes.notion.site/Release-Notes-6e2a338ee0ee426490654cded0706c92?pvs=4"
-          className="text-xs text-zinc-400 underline-offset-4 hover:underline md:text-sm dark:text-zinc-500"
+          className="text-xs text-zinc-500 underline-offset-4 hover:underline dark:text-zinc-500 md:text-sm"
         >
           Novidades
         </a>
         <a
           href="https://forms.gle/ruRh4v4UHHMthkQ38"
-          className="text-xs text-zinc-400 underline-offset-4 hover:underline md:text-sm dark:text-zinc-500"
+          className="text-xs text-zinc-500 underline-offset-4 hover:underline dark:text-zinc-500 md:text-sm"
         >
           Feedback
         </a>
