034d7aa22252e10a6db76ceb1bdb8b86e3bbc59d
Gabriel Alves de Paiva <gabriel.paiva@fastsolutions.com.br>
2024-10-09 19:15:13 -0300

    chore: add obfuscator plugin for vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vite'
+import vitePluginBundleObfuscator from 'vite-plugin-bundle-obfuscator'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vitePluginBundleObfuscator()],
   test: {
     globals: true,
     environment: 'jsdom',
